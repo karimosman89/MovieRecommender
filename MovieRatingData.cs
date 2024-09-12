446445e8141e306df5af38425faeaa3ba69edc48
karimosman89 <60973591+karimosman89@users.noreply.github.com>
2024-09-12 11:58:08 +0200

    Initial commit

---
+++ MovieRatingData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+
+
+public class MovieRating
+{
+    [LoadColumn(0)]
+    public float userId;
+    [LoadColumn(1)]
+    public float movieId;
+    [LoadColumn(2)]
+    public float Label;
+}
+
+
+public class MovieRatingPrediction
+{
+    public float Label;
+    public float Score;
+}
